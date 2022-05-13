41fc9e6b60d2c3848dadf89c1bfd5122def1cfba
Sidney Cesar <sidneyycesar@hotmail.com>
2022-05-13 18:09:14 -0300

    add web app

--- web/src/components/WidgetForm/index.tsx
+++ web/src/components/WidgetForm/index.tsx
@@ -63,7 +63,7 @@ export function WidgetForm() {
       ) }
 
       <footer className="text-xs text-neutral-400">
-        Feito com 💜 pela <a className="underline underline-offset-2" href="https://rocketseat.com.br">Rocketseat</a>
+        Sidney Cesar - Trainning <a className="underline underline-offset-2" href="https://rocketseat.com.br">Rocketseat</a>
       </footer>
     </div>
   )
